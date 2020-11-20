b3ac028ab2c2fb078b4398fdaf3be0404214b668
Anton <anthonylapin11@gmail.com>
2020-11-20 14:22:03 +0300

    search questions added

---
+++ QuestionsForum/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuestionsForum.Data;
+using QuestionsForum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionsForum.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SearchController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index(string searchQuery)
+        {
+            var questions = _db.Questions
+                .Where(q => q.Name.Contains(searchQuery) || q.Description.Contains(searchQuery))
+                .ToList();
+            var tags = _db.Tags.Include(t => t.Questions)
+                .Where(t => t.Name.Contains(searchQuery));
+            
+            foreach(var t in tags)
+            {
+                questions = questions.Union(t.Questions).ToList();
+            }
+
+            return View(questions);
+        }
+    }
+}
